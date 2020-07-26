1c5291f481ff680954fd7562acf99a7caf2b47fa
Rabbul420 <68807803+Rabbul420@users.noreply.github.com>
2020-07-26 16:10:07 +0600

    Update SmartBoy.py

--- SmartBoy.py
+++ SmartBoy.py
@@ -50,15 +50,21 @@ def jalan(z):
 logo = """
                        
                      
-▒█▀▀▀█ ▒█▀▄▀█ ░█▀▀█ ▒█▀▀█ ▀▀█▀▀ 
-░▀▀▀▄▄ ▒█▒█▒█ ▒█▄▄█ ▒█▄▄▀ ░▒█░░ 
-▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ ▒█░▒█ ░▒█░░
-       facebook:Easin.Official10
+
+╭━━━╮╱╱╭╮╱╭╮╱╱╱╱╭╮
+┃╭━╮┃╱╱┃┃╱┃┃╱╱╱╱┃┃
+┃╰━╯┣━━┫╰━┫╰━┳╮╭┫┃
+┃╭╮╭┫╭╮┃╭╮┃╭╮┃┃┃┃┃
+┃┃┃╰┫╭╮┃╰╯┃╰╯┃╰╯┃╰╮
+╰╯╰━┻╯╰┻━━┻━━┻━━┻━╯
+
+
+       facebook:.Normal.User.00420
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;98mSmart Boy Easin
-\033[1;96mInstagram \033[1;93m: \033[1: md_easin
-\033[1;96mFacebook  \033[1;93m: \033[1: Easin.Official0
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/SmartBoyEasin/SmartBoy
+\033[1;96mAuthor  \033[1;93m: \033[1;Normal.User.00420
+\033[1;96mInstagram \033[1;93m: \033[1: Rabbu-420
+\033[1;96mFacebook  \033[1;93m: \033[1: Normal.User.00420
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Rabbul420/Rabbul420
 \033[1;91m======================================="""
 
 def tik():
@@ -81,20 +87,26 @@ print "\033[1;96m ============================================================="
 print  """\033[1;91m
  
             
-▒█▀▀▀█ ▒█▀▄▀█ ░█▀▀█ ▒█▀▀█ ▀▀█▀▀ 
-░▀▀▀▄▄ ▒█▒█▒█ ▒█▄▄█ ▒█▄▄▀ ░▒█░░ 
-▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ ▒█░▒█ ░▒█░░
-       Facebook:Smart Boy Easin
+
+╭━━━╮╱╱╭╮╱╭╮╱╱╱╱╭╮
+┃╭━╮┃╱╱┃┃╱┃┃╱╱╱╱┃┃
+┃╰━╯┣━━┫╰━┫╰━┳╮╭┫┃
+┃╭╮╭┫╭╮┃╭╮┃╭╮┃┃┃┃┃
+┃┃┃╰┫╭╮┃╰╯┃╰╯┃╰╯┃╰╮
+╰╯╰━┻╯╰┻━━┻━━┻━━┻━╯
+
+
+       Facebook:Normal.User.00420
  
-\033[1;96mAuthor  \033[1;93m: \033[1;92mSmart Boy Easin
-\033[1;96mInstagram \033[1;93m: \033[1;92mmd_easin
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Easin.Official0
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/SmartBoyEasin/SmartBoy
+\033[1;96mAuthor  \033[1;93m: \033[1;Rabbul420
+\033[1;96mInstagram \033[1;93m: \033[1;Rabbul-420
+\033[1;96mFacebook  \033[1;93m: \033[1;Normal.User.00420
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Rabbul420/Rabbul420
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "easin"
-CorrectPassword = "easin"
+CorrectUsername = "rabbul"
+CorrectPassword = "Rabbul420"
 
 loop = 'true'
 while (loop == 'true'):
@@ -415,7 +427,7 @@ def pilih_super():
 	p = ThreadPool(30)
 	p.map(main, id)
 	print 42*"\033[1;96m="
-	print '\033[1;96m[\033[1;97m✓\033[1;96m] \033[1;92mProcess Has Been Completed Komail says Thank You♥️ \033[1;97m....'
+	print '\033[1;96m[\033[1;97m✓\033[1;96m] \033[1;92mProcess Has Been Completed Rabbul says Thank You♥️ \033[1;97m....'
 	print"\033[1;96m[+] \033[1;92mTotal OK/\x1b[1;93mCP \033[1;91m: \033[1;92m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
 	print("\033[1;96m[+] \033[1;92mTHANKS FOR USING MY COMMANDS ! WE WILL BE RIGHT BACK \033[1;91m: \033[1;97mout/checkpoint.txt")
 	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
